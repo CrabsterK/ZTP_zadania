d77b3ac69f9f08e8a161fcea8df4bd5abcd892dc
CrabsterK <arek.marcinowski18@gmail.com>
2018-03-09 17:00:08 +0100

    add methods to print items

--- ZTP_zadanie0/src/main/java/com/zadanie0/Item.java
+++ ZTP_zadanie0/src/main/java/com/zadanie0/Item.java
@@ -1,8 +1,6 @@
 package com.zadanie0;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Item {
     Connection myConn;
@@ -45,4 +43,92 @@ public class Item {
             e.printStackTrace();
         }
     }
+
+
+
+
+    public void printAllItems(int IdInvoice){
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "select productName, price, quantity, IdInvoice from Item";
+        try {
+            stmt = myConn.createStatement();
+            rs = stmt.executeQuery(query);
+            int lp = 1;
+            while (rs.next()) {
+                if(rs.getInt("idInvoice") == IdInvoice) {
+                    String productName = rs.getString("productName");
+                    int price = rs.getInt("price");
+                    int quantity = rs.getInt("quantity");
+                    System.out.println("=============================================================================");
+                    System.out.format("%5d%32s%15d%10d", lp, productName, price, quantity);
+                    System.out.println();
+                    lp++;
+                }
+            }
+
+            //drukuj pozycje faktury
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void showItems(int IdInvoice){
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "select IdItem, productName, IdInvoice from Item ";// + "SALES, TOTAL " + "from " + dbName + ".COFFEES";
+        try {
+            stmt = myConn.createStatement();
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                if(rs.getInt("idInvoice") == IdInvoice) {
+                    int IdItem = rs.getInt("IdItem");
+                    String productName = rs.getString("productName");
+                    System.out.println("ID" + "\t" + "Nazwa produktu");
+                    System.out.println(IdItem + "\t" + productName);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void printOneItem(int IdInvoice, int IdItem){
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "select IdItem, productName, price, quantity, IdInvoice from Item";
+        try {
+            stmt = myConn.createStatement();
+            rs = stmt.executeQuery(query);
+            int lp = 1;
+            while (rs.next()) {
+                if(rs.getInt("idInvoice") == IdInvoice && rs.getInt("IdItem") == IdItem) {
+                    String productName = rs.getString("productName");
+                    int price = rs.getInt("price");
+                    int quantity = rs.getInt("quantity");
+                    System.out.println("=============================================================================");
+                    System.out.format("%5s%32s%15s%10s", "Lp.", "Nazwa produktu", "Cena [zł]", "Ilość");
+                    System.out.println();
+                    System.out.println("=============================================================================");
+                    System.out.format("%5d%32s%15d%10d", lp, productName, price, quantity);
+                    System.out.println();
+                    System.out.println("=============================================================================");
+                    lp++;
+                }
+            }
+
+            //drukuj pozycje faktury
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
 }
