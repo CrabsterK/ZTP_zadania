8aa5a3e28d4fa2f1912a2a8e05f0cdc78928c9c8
CrabsterK <arek.marcinowski18@gmail.com>
2018-04-07 22:14:10 +0200

    repart methods

--- ZTP_zadanie2.1/src/GeneratorCarList.java
+++ ZTP_zadanie2.1/src/GeneratorCarList.java
@@ -5,40 +5,38 @@ public class GeneratorCarList {
     public GeneratorCarList() {
     }
 
-    public ArrayDataHolder generateArr(int quantity, boolean optimalCar){
-        Object[] list = new Object[quantity];
-
-        if(optimalCar == false){
-            CarSuboptimal soCar = new CarSuboptimal();
-            for(int i = 0; i < quantity; i++){
-                list[i] = soCar.generateRandom();
-            }
-        }
-
-        if(optimalCar == true){
-            CarOptimal optCar = new CarOptimal();
-            for(int i = 0; i < quantity; i++){
-                list[i] = optCar.generateRandom();
-            }
+    public ArrayDataHolder generateArrSobCar(int quantity){
+        CarSuboptimal[] list = new CarSuboptimal[quantity];
+        CarSuboptimal soCar = new CarSuboptimal();
+        for(int i = 0; i < quantity; i++){
+            list[i] = soCar.generateRandom();
         }
         return new ArrayDataHolder(list);
     }
 
-    public ArrayListDataHolder generateList(int quantity, boolean optimalCar){
-        ArrayList<Object> list = new ArrayList<Object>();
-
-        if(optimalCar == false){
-            CarSuboptimal soCar = new CarSuboptimal();
-            for(int i = 0; i < quantity; i++){
-                list.add(soCar.generateRandom());
-            }
+    public ArrayDataHolder generateArrOptCar(int quantity){
+        CarOptimal[] list = new CarOptimal[quantity];
+        CarOptimal optCar = new CarOptimal();
+        for(int i = 0; i < quantity; i++){
+            list[i] = optCar.generateRandom();
         }
+        return new ArrayDataHolder(list);
+    }
 
-        if(optimalCar == true){
-            CarOptimal optCar = new CarOptimal();
-            for(int i = 0; i < quantity; i++){
-                list.add(optCar.generateRandom());
-            }
+    public ArrayListDataHolder generateListSobCar(int quantity){
+        ArrayList<Object> list = new ArrayList<Object>();
+        CarSuboptimal soCar = new CarSuboptimal();
+        for(int i = 0; i < quantity; i++){
+            list.add(soCar.generateRandom());
+        }
+        return new ArrayListDataHolder(list);
+    }
+
+    public ArrayListDataHolder generateListOptCar(int quantity ){
+        ArrayList<Object> list = new ArrayList<Object>();
+        CarOptimal optCar = new CarOptimal();
+        for(int i = 0; i < quantity; i++){
+            list.add(optCar.generateRandom());
         }
         return new ArrayListDataHolder(list);
     }
