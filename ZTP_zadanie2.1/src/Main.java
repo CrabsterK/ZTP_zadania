d96d80cc91d5782e8ba34ad75fa9ecdcf8a80fa1
CrabsterK <arek.marcinowski18@gmail.com>
2018-04-07 23:01:56 +0200

    add testing script in main class

--- ZTP_zadanie2.1/src/Main.java
+++ ZTP_zadanie2.1/src/Main.java
@@ -4,75 +4,79 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         final int EXPERIMENT_COUNTER = 10;
-       GeneratorCarList carGenerator = new GeneratorCarList();
-        long startTime;
-        long estimatedTime;
+        final int LIST_TO_USE = 10000;
+        GeneratorCarList carGenerator = new GeneratorCarList();
+        BubbleSort bt = new BubbleSort();
+        long startTimeGenerate;
+        long startTimeSort;
+        long estimatedTimeGenerate;
+        long estimatedTimeSort;
 
-        System.out.println("====== OPTYMALIZACJA ======");
-        System.out.println("OBIEKT \tLISTA \tCZAS (mediana z " + EXPERIMENT_COUNTER + " prób)");
+
+        System.out.println("====== GENEROWANIE LIST OBIEKTÓW/SORTOWANIE ======");
+        System.out.println("OBIEKT \tLISTA \tCZAS GENEROWANIA \tCZAS SORTOWANIA (mediana z " + EXPERIMENT_COUNTER + " prób, wielkość list: " + LIST_TO_USE +")");
 
 
         ArrayList arrTT = new ArrayList();
-        for(int i = 0; i < EXPERIMENT_COUNTER; i++) {
-            startTime = System.currentTimeMillis();
-            carGenerator.generateArr(1000000, true);
-            estimatedTime = System.currentTimeMillis() - startTime;
-            arrTT.add((double)estimatedTime);
+        ArrayList arrSortTT = new ArrayList();
+        for (int i = 0; i < EXPERIMENT_COUNTER; i++) {
+            startTimeGenerate = System.nanoTime();
+            CarOptimal[] list = carGenerator.generateArrOptCar(LIST_TO_USE).getList();
+            estimatedTimeGenerate = System.nanoTime() - startTimeGenerate;
+            startTimeSort = System.currentTimeMillis();
+            bt.sortArray(list);
+            estimatedTimeSort = System.currentTimeMillis() - startTimeSort;
+            arrTT.add((double) estimatedTimeGenerate);
+            arrSortTT.add((double) estimatedTimeSort);
         }
-        System.out.println("TAK \tTAK \t" + getMedian(arrTT) + "ms");
+        System.out.println("TAK \tTAK \t" + getMedian(arrTT)/1000 + "µs \t\t\t" + getMedian(arrSortTT) + "ms");
 
 
         ArrayList arrNT = new ArrayList();
-        for(int i = 0; i < EXPERIMENT_COUNTER; i++) {
-            startTime = System.currentTimeMillis();
-            carGenerator.generateArr(1000000, false);
-            estimatedTime = System.currentTimeMillis() - startTime;
-            arrNT.add((double)estimatedTime);
+        ArrayList arrSortNT = new ArrayList();
+        for (int i = 0; i < EXPERIMENT_COUNTER; i++) {
+            startTimeGenerate = System.nanoTime();
+            CarSuboptimal[] list = carGenerator.generateArrSobCar(LIST_TO_USE).getList();
+            estimatedTimeGenerate = System.nanoTime() - startTimeGenerate;
+            startTimeSort = System.currentTimeMillis();
+            bt.sortArray(list);
+            estimatedTimeSort = System.currentTimeMillis() - startTimeSort;
+            arrNT.add((double) estimatedTimeGenerate);
+            arrSortNT.add((double) estimatedTimeSort);
         }
-        System.out.println("NIE \tTAK \t" + getMedian(arrNT) + "ms");
+        System.out.println("NIE \tTAK \t" + getMedian(arrNT)/1000 + "µs \t\t\t" + getMedian(arrSortNT) + "ms");
 
 
         ArrayList arrTN = new ArrayList();
-        for(int i = 0; i < EXPERIMENT_COUNTER; i++) {
-            startTime = System.currentTimeMillis();
-            carGenerator.generateList(1000000, true);
-            estimatedTime = System.currentTimeMillis() - startTime;
-            arrTN.add((double)estimatedTime);
+        ArrayList arrSortTN = new ArrayList();
+        for (int i = 0; i < EXPERIMENT_COUNTER; i++) {
+            startTimeGenerate = System.nanoTime();
+            ArrayList list = carGenerator.generateListOptCar(LIST_TO_USE).getList();
+            estimatedTimeGenerate = System.nanoTime() - startTimeGenerate;
+            startTimeSort = System.currentTimeMillis();
+            bt.sortList(list);
+            estimatedTimeSort = System.currentTimeMillis() - startTimeSort;
+            arrTN.add((double) estimatedTimeGenerate);
+            arrSortTN.add((double) estimatedTimeSort);
         }
-        System.out.println("TAK \tNIE \t" + getMedian(arrTN) + "ms");
+        System.out.println("TAK \tNIE \t" + getMedian(arrTN)/1000 + "µs \t\t\t" + getMedian(arrSortTN) + "ms");
 
 
         ArrayList arrNN = new ArrayList();
-        for(int i = 0; i < EXPERIMENT_COUNTER; i++) {
-            startTime = System.currentTimeMillis();
-            carGenerator.generateList(1000000, false);
-            estimatedTime = System.currentTimeMillis() - startTime;
-            arrNN.add((double)estimatedTime);
+        ArrayList arrSortNN = new ArrayList();
+        for (int i = 0; i < EXPERIMENT_COUNTER; i++) {
+            startTimeGenerate = System.nanoTime();
+            ArrayList list = carGenerator.generateListSobCar(LIST_TO_USE).getList();
+            estimatedTimeGenerate = System.nanoTime() - startTimeGenerate;
+            startTimeSort = System.currentTimeMillis();
+            bt.sortList2(list);
+            estimatedTimeSort = System.currentTimeMillis() - startTimeSort;
+            arrNN.add((double) estimatedTimeGenerate);
+            arrSortNN.add((double) estimatedTimeSort);
         }
-        System.out.println("NIE \tNIE \t" + getMedian(arrNN) + "ms");
-
-
-
-
-
-
-        /*carGenerator.generateArr(1000, true);
-        carGenerator.generateArr(10000, true);
-        carGenerator.generateArr(100000, true);
-
-        carGenerator.generateArr(1000, false);
-        carGenerator.generateArr(10000, false);
-        carGenerator.generateArr(100000, false);
-
-
-        carGenerator.generateList(1000, true);
-        carGenerator.generateList(10000, true);
-        carGenerator.generateList(100000, true);
-
-        carGenerator.generateList(1000, false);
-        carGenerator.generateList(10000, false);
-        carGenerator.generateList(100000, false);*/
+        System.out.println("NIE \tNIE \t" + getMedian(arrNN)/1000 + "µs \t\t\t" + getMedian(arrSortNN) + "ms");
     }
+
 
     public static double getMedian(ArrayList set) {
         Collections.sort(set);
